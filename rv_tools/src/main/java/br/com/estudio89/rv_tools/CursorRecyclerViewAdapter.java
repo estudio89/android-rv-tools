7b99a5e55ee19c2fc91b978c3d2b70895f6ed44c
Luccas Correa <luccascorrea@estudio89.com.br>
2016-02-14 14:51:38 -0200

    First commit.

---
+++ rv_tools/src/main/java/br/com/estudio89/rv_tools/CursorRecyclerViewAdapter.java
@@ -0,0 +1,246 @@
+package br.com.estudio89.rv_tools;
+
+import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Filter;
+import android.widget.FilterQueryProvider;
+
+/*
+ * Copyright (C) 2014 skyfish.jy@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> implements CursorFilter.CursorFilterClient {
+
+    private Cursor mCursor;
+
+    private boolean mDataValid;
+
+    private int mRowIdColumn;
+
+    private DataSetObserver mDataSetObserver;
+    private FilterQueryProvider mFilterQueryProvider;
+    private CursorFilter mCursorFilter;
+
+    public CursorRecyclerViewAdapter(Cursor cursor) {
+        mCursor = cursor;
+        mDataValid = cursor != null;
+        mRowIdColumn = mDataValid ? mCursor.getColumnIndex(getIdColumnName()) : -1;
+        mDataSetObserver = new NotifyingDataSetObserver();
+        if (mCursor != null) {
+            mCursor.registerDataSetObserver(mDataSetObserver);
+        }
+    }
+
+    public Cursor getCursor() {
+        return mCursor;
+    }
+
+    protected boolean isDataValid() {
+        return mDataValid;
+    }
+
+    protected int getPosition(int originalPosition) {
+        return originalPosition;
+    }
+
+    @Nullable
+    public Cursor getCursor(int position) {
+        position = getPosition(position);
+        if (mDataValid && mCursor != null && mCursor.moveToPosition(position)) {
+            return mCursor;
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mDataValid && mCursor != null) {
+            return mCursor.getCount();
+        }
+        return 0;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        position = getPosition(position);
+        if (mDataValid && mCursor != null && mCursor.moveToPosition(position)) {
+            return mCursor.getLong(mRowIdColumn);
+        }
+        return 0;
+    }
+
+    /**
+     * This method can be overridden by subclasses if
+     * a different column name is used for object ids.
+     * @return the name of the column used as the unique identifier.
+     */
+    protected String getIdColumnName() {
+        return "ID";
+    }
+
+    @Override
+    public void setHasStableIds(boolean hasStableIds) {
+        super.setHasStableIds(true);
+    }
+
+    public abstract void onBindViewHolder(VH viewHolder, Cursor cursor);
+
+    @Override
+    public void onBindViewHolder(VH viewHolder, int position) {
+        position = getPosition(position);
+        if (!mDataValid) {
+            throw new IllegalStateException("this should only be called when the cursor is valid");
+        }
+        if (!mCursor.moveToPosition(position)) {
+            throw new IllegalStateException("couldn't move cursor to position " + position);
+        }
+        onBindViewHolder(viewHolder, mCursor);
+    }
+
+    /**
+     * Change the underlying cursor to a new cursor. If there is an existing cursor it will be
+     * closed.
+     */
+    public void changeCursor(Cursor cursor) {
+        Cursor old = swapCursor(cursor);
+        if (old != null) {
+            old.close();
+        }
+    }
+
+    /**
+     * Swap in a new Cursor, returning the old Cursor.  Unlike
+     * {@link #changeCursor(Cursor)}, the returned old Cursor is <em>not</em>
+     * closed.
+     */
+    public Cursor swapCursor(Cursor newCursor) {
+        if (newCursor == mCursor) {
+            return null;
+        }
+        final Cursor oldCursor = mCursor;
+        if (oldCursor != null && mDataSetObserver != null) {
+            oldCursor.unregisterDataSetObserver(mDataSetObserver);
+        }
+        mCursor = newCursor;
+        if (mCursor != null) {
+            if (mDataSetObserver != null) {
+                mCursor.registerDataSetObserver(mDataSetObserver);
+            }
+            mRowIdColumn = newCursor.getColumnIndexOrThrow(getIdColumnName());
+            mDataValid = true;
+            notifyDataSetChanged();
+        } else {
+            mRowIdColumn = -1;
+            mDataValid = false;
+            notifyDataSetChanged();
+            //There is no notifyDataSetInvalidated() method in RecyclerView.Adapter
+        }
+        return oldCursor;
+    }
+
+    /**
+     * <p>Converts the cursor into a CharSequence. Subclasses should override this
+     * method to convert their results. The default implementation returns an
+     * empty String for null values or the default String representation of
+     * the value.</p>
+     *
+     * @param cursor the cursor to convert to a CharSequence
+     * @return a CharSequence representing the value
+     */
+    public CharSequence convertToString(Cursor cursor) {
+        return cursor == null ? "" : cursor.toString();
+    }
+
+    /**
+     * Sets the query filter provider used to filter the current Cursor.
+     * The provider's
+     * {@link android.widget.FilterQueryProvider#runQuery(CharSequence)}
+     * method is invoked when filtering is requested by a client of
+     * this adapter.
+     *
+     * @param filterQueryProvider the filter query provider or null to remove it
+     *
+     * @see #getFilterQueryProvider()
+     * @see #runQueryOnBackgroundThread(CharSequence)
+     */
+    public void setFilterQueryProvider(FilterQueryProvider filterQueryProvider) {
+        mFilterQueryProvider = filterQueryProvider;
+    }
+
+    public FilterQueryProvider getFilterQueryProvider() {
+        return mFilterQueryProvider;
+    }
+
+    public Filter getFilter() {
+        if (mCursorFilter == null) {
+            mCursorFilter = new CursorFilter(this);
+        }
+        return mCursorFilter;
+    }
+
+    /**
+     * Runs a query with the specified constraint. This query is requested
+     * by the filter attached to this adapter.
+     *
+     * The query is provided by a
+     * {@link android.widget.FilterQueryProvider}.
+     * If no provider is specified, the current cursor is not filtered and returned.
+     *
+     * After this method returns the resulting cursor is passed to {@link #changeCursor(Cursor)}
+     * and the previous cursor is closed.
+     *
+     * This method is always executed on a background thread, not on the
+     * application's main thread (or UI thread.)
+     *
+     * Contract: when constraint is null or empty, the original results,
+     * prior to any filtering, must be returned.
+     *
+     * @param constraint the constraint with which the query must be filtered
+     *
+     * @return a Cursor representing the results of the new query
+     *
+     * @see #getFilter()
+     * @see #getFilterQueryProvider()
+     * @see #setFilterQueryProvider(android.widget.FilterQueryProvider)
+     */
+    public Cursor runQueryOnBackgroundThread(CharSequence constraint) {
+        if (mFilterQueryProvider != null) {
+            return mFilterQueryProvider.runQuery(constraint);
+        }
+
+        return mCursor;
+    }
+
+    private class NotifyingDataSetObserver extends DataSetObserver {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            mDataValid = true;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onInvalidated() {
+            super.onInvalidated();
+            mDataValid = false;
+            notifyDataSetChanged();
+            //There is no notifyDataSetInvalidated() method in RecyclerView.Adapter
+        }
+    }
+}
---
+++ rv_tools/src/main/java/br/com/estudio89/rv_tools/DataLoadingRecyclerViewAdapter.java
@@ -0,0 +1,292 @@
+package br.com.estudio89.rv_tools;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * Created by luccascorrea on 1/21/16.
+ *
+ */
+public abstract class DataLoadingRecyclerViewAdapter<VH extends RecyclerView.ViewHolder> extends CursorRecyclerViewAdapter<VH> implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Two view types which will be used to determine whether a row should be displaying
+    // data or a Progressbar
+    public static final int VIEW_TYPE_HEADER = 0;
+    public static final int VIEW_TYPE_LOADING = 1;
+    public static final int VIEW_TYPE_ACTIVITY = 2;
+
+    private Class viewHolderClass;
+    private DataLoader dataLoadListener;
+    private ItemClickListener clickListener;
+
+    @Nullable private WeakReference<View> wkEmptyView;
+    @Nullable private WeakReference<View> wkRecyclerView;
+    @Nullable private VH headerViewHolder;
+
+    @NonNull private Context context;
+    private LoaderManager loaderManager;
+
+    public interface DataLoader {
+        void onLoadMore();
+        boolean displayProgress();
+    }
+
+    public interface ItemClickListener {
+        boolean onItemClick(View view, int position, RecyclerView.ViewHolder viewHolder);
+    }
+
+    public interface FullItemClickListener extends ItemClickListener {
+        boolean onItemLongClick(View view, int position, RecyclerView.ViewHolder viewHolder);
+    }
+
+    /**
+     * For initializing variables, don't do it in this method, see {@link #initialize(Object, LoaderManager, Object...)}.
+     * @param activity the activity where the recycler view is placed
+     * @param loaderManager getSupportLoaderManager();
+     * @param args initialization arguments
+     */
+
+    public DataLoadingRecyclerViewAdapter(@NonNull Activity activity, @NonNull LoaderManager loaderManager, Object... args) {
+        super(null);
+        this.context = activity;
+        initialize(activity, loaderManager, args);
+    }
+
+    /**
+     * For initializing variables, don't do it in this method, see {@link #initialize(Object, LoaderManager, Object...)}.
+     * @param fragment the fragment where the recycler view is placed
+     * @param loaderManager fragment.getLoaderManager();
+     * @param args initialization arguments
+     */
+    public DataLoadingRecyclerViewAdapter(Fragment fragment, @NonNull LoaderManager loaderManager, Object... args) {
+        super(null);
+        this.context = fragment.getActivity();
+        initialize(fragment, loaderManager, args);
+    }
+
+    /**
+     * When initializing variables, never do it in the constructor, always override this method,
+     * calling super at the last line.
+     *
+     * @param fragmentOrActivity the fragment of activity passed to the constructor
+     * @param loaderManager the loadermanager passed to the constructor
+     * @param args array of extra arguments passed to the constructor
+     */
+    protected void initialize(Object fragmentOrActivity, @NonNull LoaderManager loaderManager, Object... args) {
+        try {
+            this.dataLoadListener = (DataLoader) fragmentOrActivity;
+        } catch (ClassCastException e) {
+            throw new RuntimeException(((Object) fragmentOrActivity).getClass().getSimpleName() + " must implement DataLoader.");
+        }
+        this.viewHolderClass = ((Class) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[0]);
+
+
+        this.loaderManager = loaderManager;
+        this.headerViewHolder = this.createHeaderViewHolder();
+        this.loaderManager.initLoader(0, null, this);
+
+    }
+
+    public void setItemClickListener(ItemClickListener listener) {
+        this.clickListener = listener;
+    }
+
+    public void setEmptyView(@NonNull RecyclerView recyclerView, @NonNull View emptyView) {
+        wkEmptyView = new WeakReference<>(emptyView);
+        wkRecyclerView = new WeakReference<View>(recyclerView);
+
+        registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                if (wkEmptyView != null && wkRecyclerView != null) {
+                    View emptyView = wkEmptyView.get();
+                    View recyclerView = wkRecyclerView.get();
+                    if (emptyView != null && recyclerView != null) {
+                        if (getItemCount() == 1 && !shouldDisplayProgress()) {
+                            emptyView.setVisibility(View.VISIBLE);
+                            recyclerView.setVisibility(View.GONE);
+                        } else {
+                            emptyView.setVisibility(View.GONE);
+                            recyclerView.setVisibility(View.VISIBLE);
+                        }
+
+                    }
+                }
+
+            }
+        });
+    }
+
+    public void setHeaderView(View view) {
+        try {
+            Constructor constructor = this.viewHolderClass.getConstructor(this.getClass(), View.class);
+            this.headerViewHolder = (VH) constructor.newInstance(this, view);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean shouldDisplayProgress() {
+        return !isDataValid() || this.dataLoadListener.displayProgress();
+    }
+
+
+    @Override
+    public int getItemCount() {
+        // This is for adding the progress bar
+        int count = super.getItemCount() + 1;
+
+        if (this.headerViewHolder != null) {
+            count += 0;
+        }
+
+        return count;
+    }
+
+    /**
+     * This method offsets the position in case there is a header view.
+     *
+     * @param originalPosition the original position.
+     * @return new position after offset.
+     */
+    @Override
+    protected int getPosition(int originalPosition) {
+        if (headerViewHolder != null) {
+            originalPosition -= 1;
+        }
+
+        return originalPosition;
+    }
+
+    @Override
+    public void onBindViewHolder(final VH viewHolder, final int position) {
+        if (position != 0 || headerViewHolder == null) {
+            int superCount = super.getItemCount();
+            if (getPosition(position) < superCount && isDataValid()) {
+                super.onBindViewHolder(viewHolder, position);
+                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (clickListener != null) {
+                            clickListener.onItemClick(v, position, viewHolder);
+                        }
+                    }
+                });
+
+                viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        if (clickListener != null && clickListener instanceof FullItemClickListener) {
+                            FullItemClickListener fullListener = (FullItemClickListener) clickListener;
+                            fullListener.onItemLongClick(viewHolder.itemView, position, viewHolder);
+                        }
+                        return false;
+                    }
+                });
+            } else {
+                if (shouldDisplayProgress()) {
+                    viewHolder.itemView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                } else {
+                    viewHolder.itemView.setLayoutParams(new ViewGroup.LayoutParams(0,0));
+                }
+
+            }
+        }
+
+    }
+
+    protected VH createHeaderViewHolder() {
+        return null;
+    }
+
+    protected abstract VH createLoadingViewHolder(ViewGroup parent);
+
+    @Override
+    public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case VIEW_TYPE_HEADER:
+                return this.headerViewHolder;
+            case VIEW_TYPE_LOADING:
+                return createLoadingViewHolder(parent);
+            default:
+                return onCreateDataViewHolder(parent, viewType);
+        }
+    }
+
+    public abstract VH onCreateDataViewHolder(ViewGroup parent, int viewType);
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0 && headerViewHolder != null) {
+            return VIEW_TYPE_HEADER;
+        } else {
+            position = getPosition(position);
+            return (position >= super.getItemCount()) ? VIEW_TYPE_LOADING : VIEW_TYPE_ACTIVITY;
+        }
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorDataLoader(this.context, this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        changeCursor(data);
+        if (data.getCount() == 0 && this.dataLoadListener.displayProgress()) {
+            this.dataLoadListener.onLoadMore();
+        }
+    }
+
+    @NonNull
+    public Context getContext() {
+        return context;
+    }
+
+    public void refresh() {
+        this.loaderManager.restartLoader(0, null, this);
+    }
+
+    public abstract Cursor getLoaderCursor();
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        changeCursor(null);
+    }
+
+    public static class CursorDataLoader extends SimpleCursorLoader {
+        private WeakReference<DataLoadingRecyclerViewAdapter> wkAdapter;
+        public CursorDataLoader(Context context, DataLoadingRecyclerViewAdapter adapter) {
+            super(context);
+            this.wkAdapter = new WeakReference<>(adapter);
+        }
+
+        @Override
+        public Cursor loadInBackground() {
+            if (this.wkAdapter.get() != null) {
+                DataLoadingRecyclerViewAdapter adapter = this.wkAdapter.get();
+                return adapter.getLoaderCursor();
+            }
+            return null;
+        }
+    }
+}
